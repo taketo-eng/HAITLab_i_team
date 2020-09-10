e0bacdfa253096f191bec70649faf7fdb186b565
Taketo <yahiro.t.eng@gmail.com>
2020-09-10 23:30:56 +0900

    ファイルを選択したときにファイル名表示できるようにした

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,10 +3,15 @@
 (function($){
 
     $(()=>{
-
-
-
-        
+        //選択したファイルを表示するためのオブジェクト
+        const file_name = $('#selected-file');
+        //typeがfileであるオブジェクト
+        const input = $('input[type=file]');
+        //changeメソッドを使用
+        input.change(function(){
+            let file = $(this).prop('files')[0];
+            file_name.text(file.name);
+        });
     });
 
 })(jQuery);

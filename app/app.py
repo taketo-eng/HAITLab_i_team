7ae08eaaf1f5da69ba65244d19c4565b5f4cd32f
Taketo <yahiro.t.eng@gmail.com>
2020-09-10 16:43:51 +0900

    POST GETとFormを追加

--- app/app.py
+++ app/app.py
@@ -1,11 +1,23 @@
 from flask import Flask, request, render_template
+from wtforms import Form, SubmitField, FileField
 
 #Flaskを定義
 app = Flask(__name__)
 
-@app.route('/')
+class UploadForm(Form):
+    upload = FileField('あなたの顔写真')
+
+    submit = SubmitField('判定')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = UploadForm(request.form)
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('index.html', form=form)
+
+    elif request.method == 'GET':
+        return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run()

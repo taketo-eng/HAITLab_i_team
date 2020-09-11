095ebc5c9af1001edf29ee447258d9a96d52e930
Taketo <yahiro.t.eng@gmail.com>
2020-09-11 17:00:25 +0900

    それぞれ何を意味するかコメントを追加した

--- app/app.py
+++ app/app.py
@@ -9,31 +9,36 @@ import glob
 #Flaskを定義
 app = Flask(__name__)
 
+#画像の一時保存フォルダ
 save_url = './static/images/uploads/'
 
 @app.route('/')
 def predicts():
+    #uploadsフォルダ内にあるファイルを確認
     src = './static/images/uploads'
     all_file = glob.glob("%s/*" % src)
     print(all_file)
+    #uploadsフォルダ内に画像ファイルがある場合それをすべて削除
     if all_file:
         for f in all_file:
             os.remove(f)
-    else:
-        print('False')
 
     return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
+        #アップロードされたファイル取得
         file = request.files['file']
+        #画像ファイルをnumpyの配列へ変換
         image = Image.open(file)
         image = image.convert('RGB')
         data = np.asarray(image)
 
+        #numpy配列から画像へ変換
         reImg = Image.fromarray(data)
         
+        #結果の画像を一時保存
         reImg.save(save_url + file.filename)
 
         return render_template('result.html', result=file.filename)

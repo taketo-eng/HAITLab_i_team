5916b9ed73cf0a6f7df9f887c2acbb8f15ab491c
Taketo <yahiro.t.eng@gmail.com>
2020-09-17 13:17:01 +0900

    adjustment

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -12,6 +12,10 @@
             let file = $(this).prop('files')[0];
             file_name.text(file.name);
         });
+
+        //gifを動かす
+        let timestamp = new Date().getTime();
+        $('#animation').attr('src', '../images/uploads/animation.gif' + '?' + timestamp);
     });
 
 })(jQuery);
